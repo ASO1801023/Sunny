0e7384908021302973d73f4663192261f0647061
ASO2001052 <2001052@s.asojuku.ac.jp>
2020-06-26 16:08:35 +0900

    add detail

---
+++ app/src/main/java/jp/ac/asojuku/sunny/detail.kt
@@ -0,0 +1,12 @@
+package jp.ac.asojuku.sunny
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
