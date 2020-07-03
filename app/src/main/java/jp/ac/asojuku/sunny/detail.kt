93fa68fefbc13f235d5e07aaed8f7ae73a535a20
ASO2001052 <2001052@s.asojuku.ac.jp>
2020-07-03 16:41:18 +0900

    edited ditail.kt

--- app/src/main/java/jp/ac/asojuku/sunny/detail.kt
+++ app/src/main/java/jp/ac/asojuku/sunny/detail.kt
@@ -2,11 +2,23 @@ package jp.ac.asojuku.sunny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class detail : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        this.ameritu_score.setOnClickListener{
+            val fragment = fragment_ameritu_score();
+            val fragmentManager = this.supportFragmentManager;
+            val fragmentTransaction = fragmentManager.beginTransaction();
+
+        }
     }
 }
