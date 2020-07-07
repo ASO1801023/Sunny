aa07907219dca5afc593a6c65e6375471eff0df5
ASO2001052 <2001052@s.asojuku.ac.jp>
2020-07-07 10:22:39 +0900

    rename drawable & edited detail.kt

--- app/src/main/java/jp/ac/asojuku/sunny/detail.kt
+++ app/src/main/java/jp/ac/asojuku/sunny/detail.kt
@@ -15,33 +15,33 @@ class detail : AppCompatActivity() {
         super.onResume()
         //雨率スコア
         this.ameritu_score.setOnClickListener{
-            val fragment = fragment_ameritu_score();
-            val fragmentManager = this.supportFragmentManager;
-            val fragmentTransaction = fragmentManager.beginTransaction();
+            val fragment = fragment_ameritu_score()
+            val fragmentManager = this.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
                 .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit();//トランザクション完了
+                .commit()//トランザクション完了
 
 
         }
         //ランキング
         this.ranking.setOnClickListener{
-            val fragment = fragment_ranking();
-            val fragmentManager = this.supportFragmentManager;
-            val fragmentTransaction = fragmentManager.beginTransaction();
+            val fragment = fragment_ranking()
+            val fragmentManager = this.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
                 .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit();//トランザクション完了
+                .commit()//トランザクション完了
 
         }
         //総降水量
         this.total_precipitation.setOnClickListener{
-            val fragment = fragment_total_precipitation();
-            val fragmentManager = this.supportFragmentManager;
-            val fragmentTransaction = fragmentManager.beginTransaction();
+            val fragment = fragment_total_precipitation()
+            val fragmentManager = this.supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
                 .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit();//トランザクション完了
+                .commit()//トランザクション完了
 
         }
 
