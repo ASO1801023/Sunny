d678a970e988acabd9759117aae73768dde5bbce
ASO2001052 <2001052@s.asojuku.ac.jp>
2020-07-15 15:52:06 +0900

    雨率スコア画面完了

--- app/src/main/java/jp/ac/asojuku/sunny/DetailActivity.kt
+++ app/src/main/java/jp/ac/asojuku/sunny/DetailActivity.kt
@@ -1,8 +1,10 @@
 package jp.ac.asojuku.sunny
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+
 
 class DetailActivity : AppCompatActivity() {
 
@@ -15,23 +17,46 @@ class DetailActivity : AppCompatActivity() {
         super.onResume()
 
         this.ameritu_score.setOnClickListener{
-            val fragment = fragment_ameritu_score()
-            val fragmentManager = this.supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
-                .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit()//トランザクション完了
+            val score = (36..65).random()
 
-
+            if (score <= 45) { //晴れ
+                val fragment = AmerituSunnyFragment()
+                val fragmentManager = this.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
+                    .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
+                    .commit()//トランザクション完了
+            } else if (score in 46..55){ //普通
+                val fragment = AmerituNomalFragment()
+                val fragmentManager = this.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container,fragment)
+                    .addToBackStack(null)
+                    .commit()
+            } else if (score >= 56){ //雨
+                val fragment = AmerituRainyFragment()
+                val fragmentManager = this.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container,fragment)
+                    .addToBackStack(null)
+                    .commit()
+            } else { //データなし
+                val fragment = UnknownFragment()
+                val fragmentManager = this.supportFragmentManager
+                val fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container,fragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
         }
         //ランキング
         this.ranking.setOnClickListener{
             val fragment = fragment_ranking()
             val fragmentManager = this.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
-                .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit()//トランザクション完了
+            fragmentTransaction.replace(R.id.container,fragment)
+                .addToBackStack(null)
+                .commit()
 
         }
         //総降水量
@@ -39,9 +64,9 @@ class DetailActivity : AppCompatActivity() {
             val fragment = fragment_total_precipitation()
             val fragmentManager = this.supportFragmentManager
             val fragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.container,fragment)//画面の「id:container」の部分にフラグメントを切り替え
-                .addToBackStack(null)//元のフラグメントをバックスタックに保存（今回は何もしない ）
-                .commit()//トランザクション完了
+            fragmentTransaction.replace(R.id.container,fragment)
+                .addToBackStack(null)
+                .commit()
 
         }
 
